c495fb8e75d304af0dea55bdda6ed88450d38118
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-04 19:26:14 -0300

    [useEffect], for charge one phrase of init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Phrase from './components/Phrase'
 
@@ -25,29 +25,37 @@ const Button = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size 0.8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `
 
 // End Styled Components
 
-
-
 function App() {
-
   //State of Phrases
   const [phrase, setPhrase] = useState({})
 
-
+  // Query API
   const handleClick = async () => {
-    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    const api = await fetch(
+      'https://breaking-bad-quotes.herokuapp.com/v1/quotes',
+    )
     const phrase = await api.json()
     setPhrase(phrase[0])
   }
 
+  // Loading phrase
+  useEffect(() => {
+    handleClick()
+  }, [])
+
   return (
     <Conteiner>
-      <Phrase
-      phrase ={phrase}
-      />
+      <Phrase phrase={phrase} />
       <Button onClick={() => handleClick()}>Get Phrase</Button>
     </Conteiner>
   )
